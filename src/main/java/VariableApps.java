d78785f70049155fb935c5d3db51c5d8795dcd69
andy <andyt.cyb@gmail.com>
2026-04-09 13:45:23 -0400

    chore: initial commit

---
+++ src/main/java/VariableApps.java
@@ -0,0 +1,11 @@
+import java.math.BigDecimal;
+
+public class VariableApps {
+    public static void main(String[] args) {
+        boolean hasPet = false;
+        int daysInWeek = 7;
+        BigDecimal coffeePrice = 4.99;
+
+
+    }
+}
